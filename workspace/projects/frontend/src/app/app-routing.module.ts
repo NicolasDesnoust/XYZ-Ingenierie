731a188565274b534fdaf2040a6c7a174d2d473c
NicolasDesnoust <desnoust.nicolas451@gmail.com>
2020-11-26 14:46:16 +0100

    Refactor: extraction du layout principal de AppComponent
    Ce changement va permettre d'appliquer des layouts différents aux routes (ex: layout sans barre de navigation pour la page de login)

--- workspace/projects/frontend/src/app/app-routing.module.ts
+++ workspace/projects/frontend/src/app/app-routing.module.ts
@@ -7,21 +7,28 @@ import { AuthGuard } from './core/auth/guards/auth.guard';
 import { CanManageAccountsGuard } from './core/auth/guards/can-manage-accounts.guard';
 import { NotAuthorizedComponent } from './core/errors/not-authorized.component';
 import { NotFoundComponent } from './core/errors/not-found.component';
+import { MainLayoutComponent } from './layouts/containers/main-layout/main-layout.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'references' },
   {
-    path: 'manage-accounts',
-    loadChildren: () =>
-      import('./features/accounts/accounts.module').then((m) => m.AccountsModule),
-    canActivate: [CanManageAccountsGuard],
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: 'manage-accounts',
+        loadChildren: () =>
+          import('./features/accounts/accounts.module').then((m) => m.AccountsModule),
+        canActivate: [CanManageAccountsGuard],
+      },
+      {
+        path: 'references',
+        loadChildren: () =>
+          import('./features/references/references.module').then((m) => m.ReferencesModule),
+      },
+      { path: 'about', component: AboutComponent },
+    ],
   },
-  {
-    path: 'references',
-    loadChildren: () =>
-      import('./features/references/references.module').then((m) => m.ReferencesModule),
-  },
-  { path: 'about', component: AboutComponent },
   { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
   { path: 'not-authorized', component: NotAuthorizedComponent },
   { path: '**', component: NotFoundComponent },
